483b31d9c173830883d99540ddf169a5c528f00c
Dimitar Dobchev <ddobchev@gmail.com>
2021-01-16 16:47:32 +0200

    logger format

--- src/Logger/LineFormat.php
+++ src/Logger/LineFormat.php
@@ -8,7 +8,6 @@ use Phalcon\Logger\Formatter\AbstractFormatter;
 
 class LineFormat extends AbstractFormatter
 {
-    protected string $date_format;
     protected string $format;
     private string $uid;
 
@@ -20,15 +19,16 @@ class LineFormat extends AbstractFormatter
      */
     public function __construct(string $uid)
     {
-        $this->setDateFormat('c');
+        $this->setDateFormat('D M j H:i:s Y');
         $this->uid = $uid;
     }
 
     public function format( \Phalcon\Logger\Item $item ): string
     {
-        $s = $this->getFormattedDate();
-        $s .= ' ['.$this->uid.']';
-        $s .= ' ['.$item->getName().']';
+        $s = '['.$this->getFormattedDate().']';
+	    $s .= ' ['.$_SERVER['REMOTE_ADDR'].']:'.$_SERVER['REMOTE_PORT'];
+	    $s .= ' ['.$this->uid.']';
+	    $s .= ' ['.$item->getName().']';
         $s .= ' - '.$item->getMessage();
         return $s;
     }
